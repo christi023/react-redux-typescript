bfe1431e8c5c242f5bd549718ed1f0961d60be6a
Sheliann Olsson <sheliannohlsson@yahoo.com>
2021-11-13 12:35:35 +0100

    index for combine reducers

---
+++ src/state/reducers/index.tsx
@@ -0,0 +1,9 @@
+// combining reducers
+import { combineReducers } from 'redux';
+import bankReducer from './bankReducer';
+
+const reducers = combineReducers({
+  bank: bankReducer,
+});
+
+export default reducers;
